7f7af4985ae96ab8c7261263f7981fe7c4a69e85
Joinemm <joonas@rautiola.co>
2022-04-13 21:30:23 +0300

    create react app

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import App from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
